1d459da9d0cf0768436b81ecce77e34c6404bb71
TaphieM <taffymayen@gmail.com>
2019-05-22 16:59:57 -0500

    primera versión del template de visitante

---
+++ src/assets/views/TemplateVisitorRegistration.js
@@ -0,0 +1,26 @@
+
+
+document.getElementById('containervisitors').innerHTML=    
+        `
+        <div class="templatevisitors">
+            <article id="visitorinfo">   
+                <label>Nombre y Apellido:</label>                     
+                <input type="name" id="visitorname" placeholder="Ingresa tu nombre y apellido"/>
+                <p id="nameerror"></p>
+                <label>Email:</label>  
+                <input type="email" id= "visitoremail" placeholder="Ingresa tu email"/>
+                <p id ="emailerror"></p>
+                <label for="inputState">Empresa que visita:</label>
+                <select id="inputState" class="form-control">
+                <option selected>Selecciona...</option>
+                <option>...</option>
+                </select>
+                <label for="inputState">Persona que visita:</label>
+                <select id="inputState" class="form-control">
+                <option selected>Selecciona...</option>
+                <option>...</option>
+                </select>
+                <button id="registration">Registrarse</button>        
+            </article>            
+        </div>
+        `;      
