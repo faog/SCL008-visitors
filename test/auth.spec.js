ceebf5fdf12457df1d7b76b9d005d5425cf51e4f
faog <fabiola.orellana.g@gmail.com>
2019-05-23 12:08:28 -0400

    Modificación de los test, de acuerdo las validaciones realizadas

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -1,11 +1,20 @@
-import { validateInput, validateEmail, validateNewCoworker } from "./../src/assets/js/validate.js";
+import { validateInput, validateEmail, validateNewCoworker, validateRegistration } from "./../src/assets/js/validate.js";
 
+/*1) Validaciones para validateRegistration()*/
+describe("validateRegistration",()=>{
+    it('deberia retornar false, si el usuario no completa ningun campo solicitado',()=>{
+        expect(validateRegistration("","","","")).toBe(false);
+    })
 
-describe('validateInput', () => {
-    it('deberia retornar false, si el usuario no completa ningun campo solicitado', () => {
-        expect(validateInput("", "", "", "")).toBe(false);
-    });
-});
+    it('deberia retornar true, si el usuario ingresa todos los campos',()=>{
+        expect(validateRegistration("Fabiola","Orellana","fabiola.orellana.g@gmail.com","123456789"))
+        .toBe(true);
+    })
+
+    it('deberia retornar false, si el usuario no completa correo y telefono',()=>{
+        expect(validateRegistration("Fabiola","Orellana","","")).toBe(false);
+    })
+})
 
 describe('validateEmail', () => {
     it('Debería retornar false, si el correo no es valido', () => {
@@ -16,8 +25,10 @@ describe('validateEmail', () => {
     });
 });
 
+/*
 describe('validateNewCoworker', () => {
     it('deberia retornar false, si este campo no esta completado', () => {
         expect(validateNewCoworker("")).not.toBe(true);
     });
 });
+*/
