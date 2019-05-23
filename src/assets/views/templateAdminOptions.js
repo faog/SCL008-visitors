f95f7c55a537f054c19d780fc9ee5c1befc8638f
FabyTapia <ftapiamorales@gmail.com>
2019-05-23 16:35:57 -0400

    solucion de problemas

--- src/assets/views/templateAdminOptions.js
+++ src/assets/views/templateAdminOptions.js
@@ -1,7 +1,7 @@
 export const templateAdminOptions = () => {
 
     document.getElementById('containervisitors').innerHTML =
-    `
+        `
     <div class="templatesession">
         <button id="newregister">Nuevo Registro</button>   
         <button id="visitorsrecord">Historial de Visitantes</button>   
