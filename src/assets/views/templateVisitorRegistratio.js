149d2fddc2f42bdfd6fbd330ad2c720e8e172641
faog <fabiola.orellana.g@gmail.com>
2019-05-22 20:42:46 -0400

    Se separa el campo de nombre y apellido, y se agrega el campo de telefono

---
+++ src/assets/views/templateVisitorRegistratio.js
@@ -0,0 +1,33 @@
+export const templateVisitorRegistration = () =>{
+    
+    document.getElementById('containervisitors').innerHTML=    
+    `
+    <div class="templatevisitors">
+        <article id="visitorinfo">   
+            <label>Nombre:</label>                     
+            <input type="text" id="visitorfirstname" placeholder="Ingresa tu nombre"/>
+            <p id="firstnameerror"></p>
+            <label>Apellido:</label>                     
+            <input type="text" id="visitorlastname" placeholder="Ingresa tu apellido"/>
+            <p id=lastnameerror"></p>
+            <label>Email:</label>  
+            <input type="email" id= "visitoremail" placeholder="Ingresa tu email"/>
+            <p id ="emailerror"></p>
+            <label>N° Celular:</label>  
+            <input type="text" id="visitorphone" placeholder="Ingresa tu n° celular"/>
+            <p id ="phoneerror"></p>
+            <label for="inputstate">Empresa que visita:</label>
+            <select id="inputstate" class="form-control">            
+            <option selected>Selecciona...</option>
+            <option>...</option>
+            </select>
+            <label for="inputState">Persona que visita:</label>
+            <select id="inputState" class="form-control">
+            <option selected>Selecciona...</option>
+            <option>...</option>
+            </select>
+            <button id="registration">Registrarse</button>        
+        </article>            
+    </div>
+    `;      
+}
