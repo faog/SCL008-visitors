f5f3355f746bb3cfbca63414e37a99ce7a906bb1
TaphieM <taffymayen@gmail.com>
2019-05-23 23:28:10 -0500

    creación de template statistics, estilo en template de historial, se conectaron estos templates

--- src/assets/views/templateStatistics.js
+++ src/assets/views/templateStatistics.js
@@ -1,4 +1,4 @@
-export const templateVisitorsRecord = () => {
+export const templateStatistics = () => {
 
     document.getElementById('containervisitors').innerHTML =
 
@@ -25,24 +25,5 @@ export const templateVisitorsRecord = () => {
           </ul>
         </div>
       </nav> 
-    <div id=cardbody class="card mb-3" style="max-width: 540px;">
-    <div class="row no-gutters">
-      <div class="col-md-4">
-        <img src="/assets/img/user-icon.png" class="card-img" alt="...">
-        
-      </div>
-      <div class="col-md-8">
-        <div id=cardinfo class="card-body">
-          <p class="card-text">Nombre:</p>
-          <p class="card-text">Apellidos:</p>
-          <p class="card-text">Número celular:</p>
-          <p class="card-text">Empresa que visita:</p>
-          <p class="card-text">Persona que visita:</p>
-          <p class="card-text"><small class="text-muted">Hr entrada:</small></p>
-          <p class="card-text"><small class="text-muted">Hr salida:</small></p>
-          
-        </div>
-      </div>
-    </div>
-  </div>`
+    `
 };
