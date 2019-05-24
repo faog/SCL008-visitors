0a391e2d585e830092b981677572e9d6a26e36f2
FabyTapia <ftapiamorales@gmail.com>
2019-05-23 21:20:17 -0400

    se trabaja en los test

--- src/assets/js/validate.js
+++ src/assets/js/validate.js
@@ -1,40 +1,36 @@
-export const validateRegistration = (visitorFirstName,visitorLastName,visitorEmail,visitorPhone) => {
-    if(visitorFirstName === ""|| visitorLastName ==="" || visitorEmail===""  || !validateEmail(visitorEmail
-        || visitorPhone==="" || !validatePhone(visitorPhone) || visitorPhone.length<9 || visitorPhone.length>9)){
-        return false;
-    }else{
-        return true;
-    }
-}
-
-/*
-export const validateInput = (fullName, email, visitCompany, whoVisit) => {
-    if (fullName === "" || email === "" || visitCompany === "" || whoVisit === "") {
+export const validateRegistration = (visitorFirstName, visitorLastName, visitorEmail, visitorPhone) => {
+    if (visitorFirstName === "" || visitorLastName === "" || visitorEmail === "" || !validateEmail(visitorEmail ||
+            visitorPhone === "" || !validatePhone(visitorPhone) || visitorPhone.length < 9 || visitorPhone.length > 9)) {
         return false;
     } else {
         return true;
     }
-};
-*/
+}
+
 export const validateEmail = (visitorEmail) => {
     //expresión regular que simula el patrón del correo electrónico
     let pattern = /\S+@\S+\.\S+/;
     return pattern.test(visitorEmail);
 };
 
-/*
+
 export const validatePhone = (visitorPhone) => {
     //expresión regular que permite solo el ingreso de números
-    let pattern = [0-9];
+    let pattern = [0 - 9];
     return pattern.test(visitorPhone);
 };
-*/
-/*
-export const validateNewCoworker = (companyName) => {
-    if (companyName === "") {
+
+export const validateNewCoworker = (coWorkerFirstName, coWorkerLastName, coWorkerPhone) => {
+    if (coWorkerFirstName === "" || coWorkerLastName === "" || coWorkerEmail === "" || !validateEmail(coWorkerEmail ||
+            visitorPhone === "" || !validatePhone(coWorkerPhone) || coWorkerPhone.length < 9 || coWorkerPhone.length > 9)) {
         return false;
     } else {
         return true;
     }
+}
+
+export const validateEmailWork = (CoWorkerNewEmail) => {
+    //expresión regular que simula el patrón del correo electrónico
+    let pattern = /\S+@\S+\.\S+/;
+    return pattern.test(CoWorkerNewEmail);
 };
-*/
