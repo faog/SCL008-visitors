07267c405a476ba5fe236097dc497b9d57400acc
faog <fabiola.orellana.g@gmail.com>
2019-05-23 20:11:37 -0400

    Solucion del error del boton de registro de empleados

--- src/assets/views/templateNewCoworker.js
+++ src/assets/views/templateNewCoworker.js
@@ -26,23 +26,24 @@ export const templateNewCoworker = () => {
         </ul>
       </div>
     </nav>
-     <div class="templatevisitors">
-        <article id="visitorinfo">   
-            <input type="text" id="coworkerfirstname" class="form-control" placeholder="Ingresa nombre"/>
+    <div class="templatevisitors">
+        <article id="visitorinfo"> 
+          <h1>Registro de empleados</h1>
+            <input type="text" id="coworkerfirstname" class="form-control" placeholder="Nombre"/>
             <p id="firstnamealert"></p>
-            <input type="text" id="coworkerlastname"class="form-control"  placeholder="Ingresa apellido"/>
+            <input type="text" id="coworkerlastname"class="form-control"  placeholder="Apellidos"/>
             <p id="lastnamealert"></p>
-            <input type="email" id= "coworkeremail" class="form-control" placeholder="Ingresa email"/>
+            <input type="email" id= "coworkeremail" class="form-control" placeholder="Correo electrónico"/>
             <p id ="emailalert"></p>
-            <input type="text" id="coworkerphone" class="form-control" placeholder="Ingresa n°de celular"/>
+            <input type="text" id="coworkerphone" class="form-control" placeholder="Número de celular"/>
             <p id ="phonealert"></p>
             <select id="inputstate" class="form-control">            
-            <option selected>Selecciona...</option>
-
-            <option>compañia 1</option>  
-            <option>compañia 2</option>
-            <option>compañia 3</option> 
-            <button id="registration">Registrarse</button>  
+              <option selected>Empresa que pertenece</option>
+              <option>Empresa 1</option>  
+              <option>Empresa 2</option>
+              <option>Empresa 3</option> 
+            </select>
+            <button id="registrationcowork">Registrar</button>  
         </article> 
     </div>
     `;
